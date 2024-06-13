cef01bdf383ef869b5dfa5324ad335d2ac27b1b8
sidhantGhising1 <154746630+sidhantGhising1@users.noreply.github.com>
2024-06-13 09:45:09 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "sidghising@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kathmandu"
+let accountState;
+
+// accountId = 2 
+
+
+accountEmail = "babybaby@gmail.com"
+accountPassword = "5733"
+accountCity = "Pokhara"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
