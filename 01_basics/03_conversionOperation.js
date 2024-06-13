bed1cb7af26af77794efb0781d0b6e0cdcede926
sidhantGhising1 <154746630+sidhantGhising1@users.noreply.github.com>
2024-06-13 10:16:59 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = 33 
+
+console.log(typeof score);
+
+
+// "33" => 33
+// "33abc" =>  NaN
+// true = 1 
+
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// "" => false
+// 1 => true
+// "Sidhant" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
